f4b20f40d688c7d99bdbdc734188ecc2c19e097c
Alexei Lozovsky <alexei@cossacklabs.com>
2020-07-28 20:43:05 +0300

    basic podspec
    use dynamic frameworks
    
    static spec
    
    spec-generating teemmplate
    
    make gh repo common
    
    actually
    
    spspsp

--- scripts/update-specs.sh
+++ scripts/update-specs.sh
@@ -9,10 +9,17 @@
 # Environment variables:
 #
 #     OUTPUT        output directory            (default: output)
+#     MIN_IOS_SDK   minimum iOS SDK version     (default: 8.0)
+#     MIN_OSX_SDK   minimum macOS SDK version   (default: 10.9)
 
 set -eu
 
 OUTPUT=${OUTPUT:-output}
+MIN_IOS_SDK=${MIN_IOS_SDK:-8.0}
+MIN_OSX_SDK=${MIN_OSX_SDK:-10.9}
+
+# GitHub repository where build script and binaries are hosted
+GITHUB_REPO="https://github.com/cossacklabs/openssl-apple"
 
 die() {
     echo 2>&1 "$@"
@@ -37,9 +44,27 @@ do
     else
         (
             head -1 "$spec" 2> /dev/null || echo "{"
-            echo "    \"$version\": \"https://github.com/cossacklabs/openssl-apple/releases/download/v$version/$package\","
+            echo "    \"$version\": \"$GITHUB_REPO/releases/download/v$version/$package\","
             tail +2 "$spec" 2> /dev/null || echo "}"
         ) > "$OUTPUT/tmp.spec"
         mv "$OUTPUT/tmp.spec" "$spec"
     fi
 done
+echo "Updated carthage/*.json"
+echo
+
+# CocoaPods does not support static frameworks and use only dynamic ones.
+podspec="cocoapods/CLOpenSSL.podspec"
+iphone_archive="openssl-dynamic-iPhone.zip"
+macosx_archive="openssl-dynamic-MacOSX.zip"
+sed -e "s/%%OPENSSL_VERSION%%/$version/g" \
+    -e "s!%%GITHUB_REPO%%!$GITHUB_REPO!g" \
+    -e "s/%%MIN_IOS_SDK%%/$MIN_IOS_SDK/g" \
+    -e "s/%%MIN_OSX_SDK%%/$MIN_OSX_SDK/g" \
+    -e "s/%%IPHONE_ARCHIVE_NAME%%/$iphone_archive/g" \
+    -e "s/%%IPHONE_ARCHIVE_HASH%%/$(shasum -a 256 "$OUTPUT/$iphone_archive" | awk '{print $1}')/g" \
+    -e "s/%%MACOSX_ARCHIVE_NAME%%/$macosx_archive/g" \
+    -e "s/%%MACOSX_ARCHIVE_HASH%%/$(shasum -a 256 "$OUTPUT/$macosx_archive" | awk '{print $1}')/g" \
+    $podspec.template > $podspec
+echo "Updated $podspec"
+echo
